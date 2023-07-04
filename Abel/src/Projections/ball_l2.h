dce4bc2c88b4e4a04dbf1474c901f6edd15c71fc
N-Kulagin <n.kulagin.it@gmail.com>
2023-07-04 19:53:34 +0300

    Renamed functions and removed unnecessary cast

--- Abel/src/Projections/ball_l2.h
+++ Abel/src/Projections/ball_l2.h
@@ -1,24 +1,24 @@
 #pragma once
 
-#include <Eigen/Core>
+#include "Eigen/Dense"
 
 template <typename Derived>
-void ProjL2Ball_InPlace(Eigen::MatrixBase<Derived>const& vec, double radius = 1.0) {
+void L2BallProjection_InPlace(Eigen::MatrixBase<Derived>const& vec, double radius = 1.0) {
 	double norm_squared = vec.squaredNorm();
 	if (norm_squared <= radius) return;
 	const_cast<Eigen::MatrixBase<Derived>&>(vec) *= radius / sqrt(norm_squared); // cast to reference for block or matrix column inputs
 }
 
 template <typename Derived>
-void ProjL2Ball_InPlace(Eigen::ArrayBase<Derived>const& vec, double radius = 1.0) {
+void L2BallProjection_InPlace(Eigen::ArrayBase<Derived>const& vec, double radius = 1.0) {
 	double norm_squared = vec.matrix().squaredNorm();
 	if (norm_squared <= radius) return;
 	const_cast<Eigen::ArrayBase<Derived>&>(vec) *= radius / sqrt(norm_squared);
 }
 
 template <typename Derived1, typename Derived2>
-void ProjL2Ball(const Eigen::MatrixBase<Derived1>const& vec, Eigen::MatrixBase<Derived2> const& result, double radius = 1.0) {
-	const_cast<Eigen::MatrixBase<Derived2>&>(result) = const_cast<const Eigen::MatrixBase<Derived1>&>(vec);
+void L2BallProjection(const Eigen::MatrixBase<Derived1>const& vec, Eigen::MatrixBase<Derived2> const& result, double radius = 1.0) {
+	const_cast<Eigen::MatrixBase<Derived2>&>(result) = vec;
 	double norm_squared = result.squaredNorm();
 	if (norm_squared <= radius) return;
 	const_cast<Eigen::MatrixBase<Derived2>&>(result) *= radius / sqrt(norm_squared);
