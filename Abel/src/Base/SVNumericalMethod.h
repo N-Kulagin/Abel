6d236f6dd23605999114d2f05ed39d486c9a7aab
N-Kulagin <n.kulagin.it@gmail.com>
2023-07-05 15:26:34 +0300

    Moved result field to the base class and changed newton class accordingly

--- Abel/src/Base/SVNumericalMethod.h
+++ Abel/src/Base/SVNumericalMethod.h
@@ -7,10 +7,11 @@ protected:
 	size_t max_iter;
 	size_t iter_counter;
 	double starting_point;
+	double result;
 	bool wasRun;
 
-	SVNumericalMethod(double tol = 0.01, size_t max_iter = 100, double starting_point = 1.0, bool wasRun = 0, size_t iter_counter = 0) :
-		tol(tol), max_iter(max_iter), iter_counter(iter_counter), starting_point(starting_point), wasRun(wasRun) {}
+	SVNumericalMethod(double tol = 0.01, size_t max_iter = 100, double starting_point = 1.0, bool wasRun = 0, size_t iter_counter = 0, double result = 0.0) :
+		tol(tol), max_iter(max_iter), iter_counter(iter_counter), starting_point(starting_point), wasRun(wasRun), result(result) {}
 
 public:
 	virtual bool was_run() const final { return wasRun; };
--- Abel/src/RootFinding/Newton.h
+++ Abel/src/RootFinding/Newton.h
@@ -10,18 +10,17 @@ private:
 	std::function<double(double)> f;
 	std::function<double(double)> f_prime;
 	double heuristic;
-	double result;
 
 public:
 
 	SVNewton(const std::function<double(double)>& f, const std::function<double(double)>& f_prime,
 		double tol = 0.01, double heuristic = 0.05, size_t max_iter = 100, double starting_point = 1.0) :
 		f(f), f_prime(f_prime), heuristic(heuristic), SVNumericalMethod(tol, max_iter, starting_point) {}
-
-	SVNewton(const SVNewton& n) : f(n.f), f_prime(n.f_prime), heuristic(n.heuristic), result(n.result),
-		SVNumericalMethod(n.tol, n.max_iter, n.starting_point, n.wasRun, n.iter_counter) {}
-
-
+	
+	SVNewton(const SVNewton& n) : f(n.f), f_prime(n.f_prime), heuristic(n.heuristic),
+		SVNumericalMethod(n.tol, n.max_iter, n.starting_point, n.wasRun, n.iter_counter, n.result) {}
+	
+	
 	SVNewton& operator=(const SVNewton& n) {
 		f = n.f;
 		f_prime = n.f_prime;
@@ -32,12 +31,12 @@ public:
 		starting_point = n.starting_point;
 		wasRun = n.wasRun;
 		result = n.result;
-
+	
 		return *this;
 	}
-
+	
 	friend std::ostream& operator<<(std::ostream&, const SVNewton&); // is friend necessary??
-
+	
 	void setParams(double tol_ = 0.01, double heuristic_ = 0.05, size_t max_iter_ = 100, double starting_point_ = 1.0) {
 		tol = tol_;
 		heuristic = heuristic_;
@@ -47,28 +46,28 @@ public:
 		result = 0.0;
 		iter_counter = 0;
 	}
-
+	
 	double getResult() { return result; }
-
+	
 	void solve() {
 		iter_counter = 0;
-
+	
 		double x_prev_prev = starting_point;
 		double x_prev = x_prev_prev;
 		double x_cur = x_prev;
-
+	
 		double f_prev = f(x_prev);
 		double f_cur = f_prime(x_prev);
-
+	
 		double f_prime_prev = f_prime(x_prev);
-
+	
 		double psi_0 = 0.0;
 		double psi_1 = 0.0;
-
+	
 		double tau = 1.0;
-
+	
 		double stop_criterion = 1.0;
-
+	
 		// one iteration
 		x_cur = x_prev - f_prev / f_prime_prev;
 		f_cur = f(x_cur);
@@ -79,23 +78,23 @@ public:
 		x_prev = x_cur;
 		++iter_counter;
 		//
-
+	
 		while (stop_criterion > tol && iter_counter < max_iter)
 		{
 			f_prev = f(x_prev);
 			f_prime_prev = f_prime(x_prev);
 			x_cur = x_prev - f_prev / f_prime_prev;
 			f_cur = f(x_cur);
-
+	
 			psi_0 = pow(f_prev, 2);
 			psi_1 = pow(f_cur, 2);
 			tau = (psi_0 + heuristic * psi_1) / (psi_0 + psi_1);
 			x_cur = x_prev - tau * f_prev / f_prime_prev;
-
+	
 			stop_criterion = abs((x_cur - x_prev) / (1.0 - (x_cur - x_prev) / (x_prev - x_prev_prev)));
 			x_prev_prev = x_prev;
 			x_prev = x_cur;
-
+	
 			++iter_counter;
 		}
 		result = x_cur;
