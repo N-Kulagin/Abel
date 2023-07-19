0c7365c4594412fb7ad440853fb75186b5cbb53c
N-Kulagin <n.kulagin.it@gmail.com>
2023-07-19 21:49:32 +0300

    Removed precompiled header from gitignore and tracking it

---
+++ Abel/src/pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Base/MVNumericalMethod.h"
+#include "Base/SVNumericalMethod.h"
+#include "MultiVarOptimization/MVGradientDescent.h"
+#include "MultiVarOptimization/MVNewton.h"
+#include "OptimizationProblems/LASSO_Result.h"
+#include "OptimizationProblems/LASSO.h"
+#include "Projections/affine.h"
+#include "Projections/ball_l1.h"
+#include "Projections/ball_l2.h"
+#include "Projections/ball_linf.h"
+#include "Projections/box.h"
+#include "Projections/orthant.h"
+#include "Projections/simplex.h"
+#include "Prox/prox_l1.h"
+#include "Prox/prox_l2.h"
+#include "Prox/prox_linf.h"
+#include "RootFinding/SVNewton.h"
+#include "RootFinding/SVNewtonGlobal.h"
+#include "SingleVarOptimization/SVGoldenSection.h"
+
+
+
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <unordered_set>
