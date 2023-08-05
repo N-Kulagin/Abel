0921bc23e6ff18f5a24f2d9ce3a1dc837b38be7d
N-Kulagin <n.kulagin.it@gmail.com>
2023-08-05 18:34:51 +0300

    Improved nelder-mead implementation

--- Abel/head/MultiVarOptimization/MVNelderMead.h
+++ Abel/head/MultiVarOptimization/MVNelderMead.h
@@ -6,7 +6,7 @@ private:
 	std::function<double(const Eigen::VectorXd& x)> f;
 	double alpha = 1.0; // reflection coefficient, alpha > 0
 	double beta = 0.5; // contraction coefficient, 0 < beta < 1
-	double gamma = 2.0; // stretching coefficient, gamma > 1 and gamma > alpha
+	double gamma = 2.0; // stretching/expansion coefficient, gamma > 1 and gamma > alpha
 	double delta = 0.5; // shrinkage coefficient, 0 < delta < 1
 	double randomCoeff = 1.0; // determines the spread of randomized initial simplex
 
@@ -22,4 +22,7 @@ public:
 
 	void setParams(double tol = 1e-3, int max_iter = 100, double randomCoeff_ = 1.0, double alpha_ = 1.0, double beta_ = 0.5, double gamma_ = 2.0, double delta_ = 0.5);
 
+private:
+	int findSecondWorst(const Eigen::ArrayXd& f_vals, double abs_max, int dimension);
+	void findMaxMin(const Eigen::ArrayXd& f_vals, double& abs_max, double& abs_min, int& worstVertexIndex, int& bestVertexIndex, int dimension);
 };
--- Abel/src/MultiVarOptimization/MVNelderMead.cpp
+++ Abel/src/MultiVarOptimization/MVNelderMead.cpp
@@ -16,9 +16,6 @@ MVNelderMead MVNelderMead::operator=(const MVNelderMead& nm)
 void MVNelderMead::solve() noexcept
 {
 	if (was_run) return;
-	// TODO: this is a naive implementation, the algorithm should not evaluate *all* function values at each iterations
-	// also smarter error computation needs to be done
-	// http://www.scholarpedia.org/article/Nelder-Mead_algorithm
 
 	Eigen::MatrixXd x(dimension, dimension + 1); // simplex verticies
 	Eigen::VectorXd centerOfMass(dimension); // center of mass
@@ -27,71 +24,92 @@ void MVNelderMead::solve() noexcept
 	Eigen::VectorXd contraction(dimension); // contraction point
 	Eigen::ArrayXd f_vals(dimension + 1); // array of function values of the simplex
 
-	x.setRandom();
-	x *= randomCoeff; // spread the randomized verticies with coefficient randomCoeff
+	if (hasStart){} // TODO: add a method to specify starting simplex
+	else {
+		x.setRandom();
+		x *= randomCoeff; // spread the randomized verticies with coefficient randomCoeff
+	}
 
 	int bestVertexIndex = 0;
-	int worstVertexIndex;
-	int secondWorstVertexIndex;
+	int worstVertexIndex = 0;
+	int secondWorstVertexIndex = -1;
 	double val = f(x.col(0));
 	double abs_min = val; // lowest function value
 	double abs_max = val; // largest function value
 	double f_reflection; // function value at reflection
+	double f_stretching; // function value at stretching point
+	double f_contraction; // function value at contraction point
 	double f_secondWorst; // second largest function value
 	double n = dimension; // dimension of the problem
 	iter_counter = 0;
 
+	for (size_t i = 0; i < n+1; i++)
+	{
+		val = f(x.col(i));
+		if (val <= abs_min) { abs_min = val; bestVertexIndex = i; } // compute values of the function at verticies and determine index of vertex with largest/smallest value
+		if (val >= abs_max) { abs_max = val; worstVertexIndex = i; }
+		f_vals(i) = val;
+	}
+	secondWorstVertexIndex = findSecondWorst(f_vals, abs_max, n + 1);
+
 	while (iter_counter < max_iter)
 	{
-		bestVertexIndex = 0;
-		worstVertexIndex = 0;
-		secondWorstVertexIndex = -1;
-		abs_min = f(x.col(0));
-		abs_max = abs_min;
-
-		for (size_t i = 0; i < n + 1; i++)
-		{
-			val = f(x.col(i));
-			if (val <= abs_min) { abs_min = val; bestVertexIndex = i; } // compute values of the function at verticies and determine index of vertex with largest/smallest value
-			if (val >= abs_max) { abs_max = val; worstVertexIndex = i; }
-			f_vals(i) = val;
-		}
-		for (size_t i = 0; i < n + 1; i++)
-		{
-			if (f_vals(i) != abs_max) {
-				if (secondWorstVertexIndex == -1) secondWorstVertexIndex = i;
-				else if (f_vals(i) > f_vals(secondWorstVertexIndex)) secondWorstVertexIndex = i; // find index of vertex with second largest function value
-			}
-		}
 		centerOfMass = (x.rowwise().sum() - x.col(worstVertexIndex)) / n;
 		val = f(centerOfMass);
-		error = sqrt(((f_vals - val).matrix().squaredNorm()) / (n + 1.0)); // can be done more efficiently
+		error = sqrt(((f_vals - val).matrix().squaredNorm()) / (n + 1.0));
 		if (error < tol) break;
 
 		reflection = (1.0 + alpha) * centerOfMass - alpha * x.col(worstVertexIndex);
 		f_reflection = f(reflection);
-		f_secondWorst = f(x.col(secondWorstVertexIndex));
+		f_secondWorst = f_vals(secondWorstVertexIndex);
 		if (f_reflection <= abs_min) {
 			stretching = (1.0 - gamma) * centerOfMass + gamma * reflection;
-			if (f(stretching) < abs_min) {
+			f_stretching = f(stretching);
+			if (f_stretching < abs_min) {
 				x.col(worstVertexIndex) = stretching;
+				abs_min = f_stretching;
+				f_vals(worstVertexIndex) = f_stretching;
 			}
 			else {
 				x.col(worstVertexIndex) = reflection;
+				abs_min = f_reflection;
+				f_vals(worstVertexIndex) = f_reflection;
 			}
+			abs_max = f_secondWorst;
+			bestVertexIndex = worstVertexIndex;
+			worstVertexIndex = secondWorstVertexIndex;
+			secondWorstVertexIndex = findSecondWorst(f_vals, abs_max, n + 1);
 		}
 		else if (f_secondWorst < f_reflection && f_reflection <= abs_max) {
 			contraction = (1.0 - beta) * centerOfMass + beta * x.col(worstVertexIndex);
+			f_contraction = f(contraction);
 			x.col(worstVertexIndex) = contraction;
+			f_vals(worstVertexIndex) = f_contraction;
+			findMaxMin(f_vals, abs_max, abs_min, worstVertexIndex, bestVertexIndex, n + 1);
+			secondWorstVertexIndex = findSecondWorst(f_vals, abs_max, n + 1);
 		}
 		else if (abs_min < f_reflection && f_reflection <= f_secondWorst) {
-			x.col(secondWorstVertexIndex) = reflection;
+			x.col(worstVertexIndex) = reflection;
+			f_vals(worstVertexIndex) = f_reflection;
+			findMaxMin(f_vals, abs_max, abs_min, worstVertexIndex, bestVertexIndex, n + 1);
+			secondWorstVertexIndex = findSecondWorst(f_vals, abs_max, n + 1);
 		}
 		else {
 			for (size_t i = 0; i < n + 1; i++)
 			{
 				x.col(i) = (1.0 - delta) * x.col(bestVertexIndex) + delta * x.col(i);
 			}
+			val = f(x.col(0));
+			abs_min = val;
+			abs_max = val;
+			for (size_t i = 0; i < n + 1; i++)
+			{
+				val = f(x.col(i));
+				if (val <= abs_min) { abs_min = val; bestVertexIndex = i; } // compute values of the function at verticies and determine index of vertex with largest/smallest value
+				if (val >= abs_max) { abs_max = val; worstVertexIndex = i; }
+				f_vals(i) = val;
+			}
+			secondWorstVertexIndex = findSecondWorst(f_vals, abs_max, n + 1);
 		}
 		++iter_counter;
 	}
@@ -111,3 +129,28 @@ void MVNelderMead::setParams(double tol_, int max_iter_, double randomCoeff_, do
 	was_run = false;
 }
 
+int MVNelderMead::findSecondWorst(const Eigen::ArrayXd& f_vals, double abs_max, int dimension)
+{
+	int secondWorstVertexIndex = -1;
+	for (size_t i = 0; i < dimension; i++)
+	{
+		if (f_vals(i) != abs_max) {
+			if (secondWorstVertexIndex == -1) secondWorstVertexIndex = i;
+			else if (f_vals(i) > f_vals(secondWorstVertexIndex)) secondWorstVertexIndex = i; // find index of vertex with second largest function value
+		}
+	}
+	return secondWorstVertexIndex;
+}
+
+void MVNelderMead::findMaxMin(const Eigen::ArrayXd& f_vals, double& abs_max, double& abs_min, int& worstVertexIndex, int& bestVertexIndex, int dimension)
+{
+	double val = f_vals(0);
+	abs_max = val;
+	abs_min = val;
+	for (size_t i = 0; i < dimension; i++)
+	{
+		val = f_vals(i);
+		if (val <= abs_min) { abs_min = val; bestVertexIndex = i; } // compute values of the function at verticies and determine index of vertex with largest/smallest value
+		if (val >= abs_max) { abs_max = val; worstVertexIndex = i; }
+	}
+}
