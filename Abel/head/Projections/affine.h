1d8d53679460e9b683941776efbeb0535a2a619d
N-Kulagin <n.kulagin.it@gmail.com>
2023-08-25 12:43:50 +0300

    Using cholesky to compute affine projection

--- Abel/head/Projections/affine.h
+++ Abel/head/Projections/affine.h
@@ -5,5 +5,5 @@
 template <typename D1, typename D2, typename D3>
 void AffineProjection(Eigen::MatrixBase<D1> const& A, Eigen::MatrixBase<D2> const& b, Eigen::MatrixBase<D3> const& input) {
 	if (A.rows() > A.cols()) throw 1;
-	const_cast<Eigen::MatrixBase<D3>&>(input) -= A.transpose() * (A * A.transpose()).inverse() * (A * input - b);
+	const_cast<Eigen::MatrixBase<D3>&>(input) -= A.transpose() * (A * A.transpose()).llt().solve(A * input - b);
 }
